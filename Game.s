a9be873c8bfa60d0879092667167a61ec04f0190
James Gilders <jamesapaloosa1@gmail.com>
2017-03-20 13:21:28 -0600

    Update Game.s

--- Game.s
+++ Game.s
@@ -1,344 +1,280 @@
-///Game Starts here
+//This details the locations of different objects in the game
 
-	.equ	SEL, 0b110111111111
-	.equ	START, 0b111011111111
-	.equ	UP, 0b111101111111
-	.equ	DOWN, 0b111110111111
-	.equ	LEFT, 0b111111011111
-	.equ	RIGHT, 0b111111101111
-	.equ	A, 0b111111110111
-BeginGame:
-                bl      init_Objects	        
-                BUTTON  .req r6		// contains the button pressed
-                JUMP    .req r7         // detects if jump is over.
-                mov	r0, #0		// initial x
-	        mov	r1, #0		// initial y
-	        ldr	r2, =1023	// final x
-	        ldr	r3, =767	// final y
-	        ldr	r4, =GameScreen
-	        bl	CreateImage
-                ldr     r1,     =MarioStandRight
-                bl      MarioPrint
-                bl      PrintObjects
-GameLoop:
-	        bl	getInput
-	        mov	BUTTON, r0
-	
-	        mov	r0, BUTTON	// arg 1: the user input
-	        ldr	r1, =RIGHT	// arg 2: desired button
-	        bl	checkButton	// check if user pressed A
-	        cmp	r0, #1	
-                bne     Left	
-                mov     r1,     #10
-                bl      MoveMarioLR
-Left:           mov     r0, BUTTON
-                ldr     r1,     =LEFT
-                bl      checkButton
-                cmp     r0,     #1
-                bne     Jump
-                mov     r1,     #-10
-                bl      MoveMarioLR
-Jump:           mov     r0,     Button
-                ldr     r1,     =A
-                bl      checkButton
-                cmp     r0, #1
-                bne     start
-                mov     r1,     #10
-                bl      MarioJump
-start:          mov     r0,     Button
-                ldr     r1,     START
-                bl      checkButton     
-                cmp     r0,     #1
-                bne     GameLoop
-                bl      StartMenu
+//Initialize Mario and Goomba to gamestart locations
+init_Objects:   push    {r0 - r10}
+                ldr     r0,     =Mario_loc
+                mov     r1,     #0
+                str     r1,     [r0], #4
+                mov     r1,     #618
+                str     r1,     [r0], #4
+                mov     r1,     #50
+                str     r1,     [r0], #4
+                mov     r1,     #618
+                str     r1,     [r0], #4
+                mov     r1,     #0
+                str     r1,     [r0], #4
+                mov     r1,     #668
+                str     r1,     [r0], #4
+                mov     r1,     #50
+                str     r1,     [r0], #4
+                mov     r1,     #668
+                str     r1,     [r0], #4
 
+                //Initialize goomba
+                ldr     r0,     =Goomba_loc
+                mov     r1,     #2622
+                str     r1,     [r0], #4
+                mov     r1,     #668
+                str     r1,     [r0], #4
+                mov     r1,     #2672
+                str     r1,     [r0], #4
+                mov     r1,     #668
+                str     r1,     [r0], #4
+                mov     r1,     #2622
+                str     r1,     [r0], #4
+                mov     r1,     #718
+                str     r1,     [r0], #4
+                mov     r1,     #2672
+                str     r1,     [r0], #4
+                mov     r1,     #718
+                str     r1,     [r0], #4
+
+                //Initialize Screen
+                ldr     r0,     =Screen_loc
+                mov     r1,     #0
+                str     r1,     [r0], #4
+                ldr     r1,     =1024
+                str     r1,     [r0]
+
+
+                pop     {r0 - r10}
+                bx      lr
+
+
+//Object codes are as follows:
+//MarioTemp 00000
+//Mario 00001
+//Goomba 00010
+//Question Box 00011
+//Unbreakable Box1 00100
+//Unbreakable Box2 00101
+//Unbreakable Box3 00110
+//Unbreakable Box4 00111
+//Pipe 01000
+//Hole 01001
+
+
+
+
+//r1 = the nubmer code of the object you want
+//r0 = address of object
+Grab:           push    {r2 - r10}
+                cmp     r1,     #0b00000
+                ldr     r0,     =Mario_Temp
+                beq     endGrab
+                cmp     r1,     #0b00001
+                ldr     r0,     =Mario_loc
+                beq     endGrab
+                cmp     r1,     #0b00010
+                ldr     r0,     =Goomba_loc
+                beq     endGrab
+                cmp     r1,     #0b00011
+                ldr     r0,     =Question1_loc
+                beq     endGrab
+                cmp     r1,     #0b00100
+                ldr     r0,     =BrickBox1_loc
+                beq     endGrab
+                cmp     r1,     #0b00101
+                ldr     r0,     =BrickBox2_loc
+                beq     endGrab
+                cmp     r1,     #0b00110
+                ldr     r0,     =BrickBox3_loc
+                beq     endGrab
+                cmp     r1,     #0b00111
+                ldr     r0,     =BrickBox4_loc
+                beq     endGrab
+                cmp     r1,     #0b01000
+                ldr     r0,     =Pipe_loc
+                beq     endGrab
+                cmp     r1,     #0b01001
+                ldr     r0,     =Hole_loc
+                beq     endGrab
+                mov     r0,     #-1
+
+endGrab:        pop     {r2 - r10}
+                bx      lr
+
+GrabImage:      push    {r2 - r10}
+                cmp     r1,     #0b00010
+                ldr     r0,     =GoombaImage
+                beq     endGrab
+                cmp     r1,     #0b00011
+                ldr     r0,     =Question1Image
+                beq     endGrab
+                cmp     r1,     #0b00100
+                ldr     r0,     =BrickBoxImage
+                beq     endGrab
+                cmp     r1,     #0b00101
+                ldr     r0,     =BrickBoxImage
+                beq     endGrab
+                cmp     r1,     #0b00110
+                ldr     r0,     =BrickBoxImage
+                beq     endGrab
+                cmp     r1,     #0b00111
+                ldr     r0,     =BrickBoxImage
+                beq     endGrab
+                cmp     r1,     #0b01000
+                ldr     r0,     =PipeImage
+                beq     endGrab
+                cmp     r1,     #0b01001
+                ldr     r0,     =HoleImage
+                beq     endGrab
+                mov     r0,     #-1
+
+endGrab:        pop     {r2 - r10}
+                bx      lr
+
+
+Grab_Screen:    ldr     r0,     Screen_loc
+
+
+//Assumes that game starts at exactly one brick box above the screen size
+//Floor will be at height 50
+
+
+
+.data
+Mario_Temp:     int.    0               //Point 1 - top left corner
+                int.    150
+
+                int.    50               //point 2 - Top right corner
+                int.    150
                 
+                int.    0               //Point 3 - bottom left corner
+                int.    50
 
+                int.    50               //Point 4 - bottom right corner
+                int.    50
+Mario_loc:      int.    0               //Point 1 - top left corner
+                int.    150
 
-
-
-
-
+                int.    50               //point 2 - Top right corner
+                int.    150
                 
+                int.    0               //Point 3 - bottom left corner
+                int.    50
 
-//====================================================
-//Draws over objects when the screen is moved
-PrintScreenMove:
-                push    {r4,    r10, lr}
-                mov     r4,     #1
-POLoop:         add     r4,     #1
-                cmp      r4,    #10
-                bge     EndPOLoop
-                mov     r10,     r1
-                mov     r0,     r4
-                bl      Grab
-                mov     r5,     r0
-                ldr	r0,     [r5], #4
-	        ldr	r1,     [r5], #20	// initial y
-	        ldr	r2,     [r5], #4 	// final x
-	        ldr	r3,     [r5]	        // final y
+                int.    50               //Point 4 - bottom right corner
+                int.    50
 
-                mov     r6,     r0
-                mov     r7,     r1
-                mov     r8,     r2
-                mov     r9,     r3
+Goomba_loc:     int.    0
+                int.    0
 
-                //Branch and link to Victor's converter        
-                //needs to provide two coordinates 
-
-                bl convertor
-                cmp     r0,     #-1
-                beq     POLoop
-                ldr     r4,     blank
-                mov     r0,     r6
-                mov     r1,     r7
-                mov     r2,     r8
-                mov     r3,     r9
-                bl      CreateImage
+                int.    0
+                int.    0
                 
-
-EndPOLoop:      pop     {r4,    r10,    lr}
-                bx      lr   
-
-
-
-
-
-//====================================================
-//Draws objects that are on the screen other than mario
-//Requires Converter NOT DONE
-PrintObjects:   push    {r4,    r10, lr}
-                mov     r4,     #1
-POLoop:         add     r4,     #1
-                cmp      r4,    #10
-                bge     EndPOLoop
-                mov     r10,     r1
-                mov     r0,     r4
-                bl      Grab
-                mov     r5,     r0
-                ldr	r0,     [r5], #4
-	        ldr	r1,     [r5], #20	// initial y
-	        ldr	r2,     [r5], #4 	// final x
-	        ldr	r3,     [r5]	        // final y
-
-                mov     r6,     r0
-                mov     r7,     r1
-                mov     r8,     r2
-                mov     r9,     r3
-
-                //Branch and link to Victor's converter        
-                //needs to provide two coordinates 
-
-                bl convertor
-                cmp     r0,     #-1
-                beq     POLoop
-                bl      GrabImage
-                mov     r4,     r0
-                mov     r0,     r6
-                mov     r1,     r7
-                mov     r2,     r8
-                mov     r3,     r9
-                bl      CreateImage
+                int.    0
+                int.    0
                 
+                int.    0
+                int.    0
 
-EndPOLoop:      pop     {r4,    r10,    lr}
-                bx      lr    
+Question1_loc:  int.    450
+                int.    468
+                
+                int.    500
+                int.    468
         
+                int.    450
+                int.    518
+        
+                int.    500
+                int.    518
 
 
+BrickBox1_loc:  int.    400
+                int.    468
 
+                int.    450
+                int.    468
 
-
-//====================================================
-//Takes in one argument, that is which mario type to print.
-MarioPrint:    push    {r4,    r10, lr}
-                mov     r10,     r1
-                mov     r0,     #0b00001
-                bl      Grab
-                mov     r5,     r0
-                ldr	r0,     [r5], #4
-	        ldr	r1,     [r5], #20	// initial y
-	        ldr	r2,     [r5], #4 	// final x
-	        ldr	r3,     [r5]	        // final y
-
-                //Branch and link to Victor's converter        
-                //needs to provide two coordinates 
-
-                bl convertor
-	        mov     r4,     r10
-	        bl	CreateImage
+                int.    400
+                int.    518
                 
-EndMarioPrint:  pop     {r4,    r10, lr}
-                bx      lr
+                int.    450
+                int.    518
 
-//====================================================
-//Prints sky over mario
-EraseMario:     push    {r2,    r10, lr}
-                mov     r0,     #0b00001
-                bl      Grab
-                mov     r5,     r0
-                ldr	r0,     [r5], #4
-	        ldr	r1,     [r5], #20	// initial y
-	        ldr	r2,     [r5], #4 	// final x
-	        ldr	r3,     [r5]	        // final y
-
-                //Branch and link to Victor's converter        
-                //needs to provide two coordinates 
-
-                bl convertor
-	        ldr     r4,     =blank
-	        bl	CreateImage
+BrickBox2_loc:  int.    500
+                int.    468
                 
-EndMarioPrint:  pop     {r2,    r10, lr}
-                bx      lr
-//===================================================
-Wait:
-                push    {r4-r10, lr}
-                ldr     r0, =0x3f003004
-                ldr     r2,     [r0]
-                add     r1,     r2                      //Time to wait is added to current time
+                int.    550
+                int.    468
+        
+                int.    500
+                int.    518
 
-Checktime:      ldr     r2,     [r0]                    //loop that decides if the desired amount of time was waited
-                cmp     r1,     r2
-                bhi     Checktime
+                int.    550
+                int.    518
 
-                pop     {r4-r10, lr}
-                bx      lr
-//===================================================
-//Takes in the direction to move in r1
-//Needs to take in if jump is true or not in r2 NOT DONE
-MoveMarioLR:
-                push    {r2,    r10,    lr}
-                bl      EraseMario
-                mov     r0,     #0b00000
-                mov     r4,     r1              //put the direction value in a safe place
-                mov     r8,     r2              //put the jump state in a safe place
-                bl      Grab
-                mov     r5,     r0
-                ldr     r0,     [r5]
-                add     r0,     r0,     r4
-                str     r0,     [r5],   #8
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
-                ldr     r2,     [r5]
-                add     r2,     r2,     r4
-                str     r2,     [r5],   #8
-                ldr     r3,     [r5]
-                add     r3,     r3,     r4
-                str     r3,     [r5],   #8
-                //Check for collision and undo movement if so
-                bl      objectCollision
-                cmp     r0,     #0
-                beq     nocol1
-                mov     r1,     r4
-                mov     r2,     r0
-                bl      CollisionHandler
-                b       EndMoveMario
+BrickBox3_loc:  int.    2298
+                int.    468
                 
+                int.    2348
+                int.    468
+        
+                int.    2298
+                int.    518
 
-nocol1          
-                mov     r0,     #0b00001
-                mov     r4,     r1              //put the direction value in a safe place
-                mov     r8,     r2              //put the jump state in a safe place
-                bl      Grab
-                mov     r5,     r0
-                ldr     r0,     [r5]
-                add     r0,     r0,     r4
-                str     r0,     [r5],   #8
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
-                ldr     r2,     [r5]
-                add     r2,     r2,     r4
-                str     r2,     [r5],   #8
-                ldr     r3,     [r5]
-                add     r3,     r3,     r4
-                str     r3,     [r5],   #8
-                cmp     r4,     #0
-                b.lt    WalkLeft
-                ldr     r1,     =MarioWalkRImg
-                b       MoveMario2
-WalkLeft:       ldr     r1,     =MarioWalkLImg
-MoveMario2      bl      MarioPrint
+                int.    2348
+                int.    518
 
-                bl      EraseMario
-                mov     r0,     #0b00000
-                bl      Grab
-                mov     r5,     r0
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
+BrickBox4_loc:  int.    2248
+                int.    468
+                
+                int.    2298
+                int.    468
+        
+                int.    2248
+                int.    518
 
-                bl      objectCollision
-                cmp     r0,     #0
-                beq     nocol2
-                mov     r1,     r4
-                mov     r2,     r0
-                bl      CollisionHandler
-                b       EndMoveMario
+                int.    2298
+                int.    518
 
-nocol2:         mov     r0,     #0b00001
-                mov     r4,     r1              //put the direction value in a safe place
-                mov     r8,     r2              //put the jump state in a safe place
-                bl      Grab
-                mov     r5,     r0
-                ldr     r0,     [r5]
-                add     r0,     r0,     r4
-                str     r0,     [r5],   #8
-                ldr     r1,     [r5]
-                add     r1,     r1,     r4
-                str     r1,     [r5],   #8
-                ldr     r2,     [r5]
-                add     r2,     r2,     r4
-                str     r2,     [r5],   #8
-                ldr     r3,     [r5]
-                add     r3,     r3,     r4
-                str     r3,     [r5],   #8
-EndMoveMarioLR:
-                cmp     r4,     #0
-                b.lt    WalkLeft
-                ldr     r1,     =MarioStandRImg
-                b       MoveMario2
-WalkLeft:       ldr     r1,     =MariStandLImg
-MoveMario2      bl      MarioPrint
-EndMoveMarioLR:
-                pop     {r4,    r10,    lr}
-                bx      lr
-//===================================================
-//Marios movement value in r1
-//Object collided with in r2
-//This method should check what kind of collision has occured. If it is with a question box,
-//the question box needs to change to a brick box and mario is pushed back to his old location
-//If it is with a brick then mario is moved back his movement amount. If it is with a goomba, 
-//from the side, mario dies. If it is with a goomba from above, Mario kills the goomba, if it
-//is a collision with ground, Mario is not moved NOT DONE
-CollisionHandler:
-                push    {r2,    r10,    lr}
-                pop     {r4,    r10,    lr}
-                bx      lr
+Pipe_loc:       int.    1224
+                int.    618
+                
+                int.    1324
+                int.    618
+        
+                int.    1224
+                int.    718
+
+                int.    1324
+                int.    718
+
+Hole_loc:       int.    1624
+                int.    718
+                
+                int.    1924
+                int.    718
+        
+                int.    1624
+                int.    768
+
+                int.    1924
+                int.    768
+
+
+Screen_loc:     int.    0
+                int.    0
+
+Victory_line:   int.    2672
+                int.    0
 
 
 
-//===================================================
-//Not Done
-MarioJump:
 
 
-
-//===================================================
-//Takes in an input that tells whether jump is true or not. If jump is false then mario can fall. If
-//Jump is true then mario will not fall. r0 = 1 is jump true, r0 = 0 is jump is false NOT DONE
-MarioFall:
 
 
 
